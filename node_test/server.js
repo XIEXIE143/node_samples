761818ffa4c73b24d3abf344ac27ed65ecb9b5c6
XIEXIE143 <xjy143@outlook.com>
2023-10-10 11:24:25 +0900

    routing

--- node_test/server.js
+++ node_test/server.js
@@ -3,12 +3,13 @@
 const express = require('express')
 // dotenvモジュール読み込み
 const dotenv = require('dotenv')
+// routesモジュール読み込み
+const routes = require('./routes')
 
 //設定読み込み
 dotenv.config()
 const HOST = process.env.HOST
 const PORT = process.env.PORT
-
 
 //Webサーバ作成
 const app = express()
@@ -21,33 +22,9 @@ app.use(express.static(__dirname + '/public'))
 // URLエンコード（マルチバイト文字の対策）
 app.use(express.urlencoded({ extended: true }))
 
-// ルーティング（URLマッピング）
-// トップページにGETリクエストされたとき処理
-// サーバを停止（ていし）: Ctrl + C
-app.get('/', (req, res) => {
-    // クライアンのからのリクエスト
-    console.log(req.body)
-    console.log(req.url)
-    console.log(req.query)
+// Routerの利用
+app.use(routes)
 
-    // サーバーからのレスポンス
-    res.send('トップページ')
-})
-
-app.get('/profile', (req, res) => {
-    res.send('プロフィールページ')
-})
-
-// ログイン認証（POST）
-app.post('/auth', (req, res) => {
-    var loginName = req.body.login_name
-    var password = req.body.password
-
-    console.log(loginName)
-    console.log(password)
-
-    res.send('ログイン')
-})
 
 // サーバー待機（たいき） wait 
 app.listen(PORT, HOST, () => {
